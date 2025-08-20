48a36919b8abc55a851f7894fc7cad50d8c3a7fc
Srinithi Sasikumar <sasikumarsrinithi09@gmail.com>
2025-08-20 09:21:10 +0530

    Operators-2

---
+++ operators2.py
@@ -0,0 +1,59 @@
+#1
+a=20
+b=7
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+#2
+r=5
+pi=3.14
+print(pi*r**2)
+import math
+print(math.pi*r**2)
+
+#3
+print(25==30)
+print(45>20)
+print(100<=99)
+
+#4
+x=True
+y=False
+print(a and y)
+print(x or y)
+print(not x)
+
+#5
+a=15
+b=25
+print(a>10 and a<20)
+print(b>20 or a<10)
+
+#6
+m=10
+n=7
+print(m&n)
+print(m|n)
+print(m^n)
+print(~m)
+
+#7
+num=10
+num+=5
+num*=2
+num-=4
+print(num)
+
+#8
+nums = [1, 2, 3, 4, 5]
+print(3 in nums)
+print(7 not in nums)
+a = [1,2,3]
+b = [1,2,3]
+print(a==b)
+print(a is b)
