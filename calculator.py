5537e047a2da50acb8594c8189ea9c0e23a99e1c
Srinithi Sasikumar <sasikumarsrinithi09@gmail.com>
2025-08-20 22:01:43 +0530

    Calculator

---
+++ calculator.py
@@ -0,0 +1,7 @@
+#calculator
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+print("Sum:",num1+num2)
+print("difference",num1-num2)
+print("product:",num1*num2)
+print("quotient:",num1//num2)
