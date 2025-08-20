dec3b6d10209d63cb97e74cf1a19187d10b393ca
Srinithi Sasikumar <sasikumarsrinithi09@gmail.com>
2025-08-20 22:10:45 +0530

    comparison

---
+++ comparison.py
@@ -0,0 +1,8 @@
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+if num1==num2:
+    print("num1 is equal to num2")
+elif(num1>num2):
+    print("num1 is greater",num1)
+else:
+    print("num2 is greater",num2)
