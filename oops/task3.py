e1a838d951d77e3a984218d773563977954d3129
Srinithi Sasikumar <sasikumarsrinithi09@gmail.com>
2025-10-07 18:22:24 +0530

    oops-1

---
+++ oops/task3.py
@@ -0,0 +1,8 @@
+class Student:
+    def students():
+        name="srinithi"
+        id=1
+        print(name)
+        print(id)
+Student.students()
+print(name)  #----NameError: name 'name' is not defined
